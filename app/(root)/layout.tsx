60497d4a8a7e4383f8a2131c8f992813601f1216
DanteTerry <sinasa88@gmail.com>
2024-02-17 02:46:04 +0100

    large device sidebar finished

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,11 @@
+import MobileNav from "@/components/Shared/MobileNav";
+import Sidebar from "@/components/Shared/Sidebar";
+
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="root">
+      <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
---
+++ app/(root)/transformations/add/notadd/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PageNotAdd() {
+  return <div>PageNotAdd</div>;
+}
+
+export default PageNotAdd;
